eb4a665bd33636597ff73650c3cb6711d6363cce
Website <Website>
2017-02-08 18:21:32 -0300

    job

--- eao/header.php
+++ eao/header.php
@@ -10,7 +10,7 @@
 	<!-- Font -->
   	<link href='https://fonts.googleapis.com/css?family=Roboto:400,700,100,100italic,300,300italic,500,500italic,700italic,900,900italic,400italic' rel='stylesheet' type='text/css'>
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
-	
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 	<?php wp_head(); ?>
 </head>
 <body>
--- eao/index.php
+++ eao/index.php
@@ -1,38 +1,24 @@
 <?php get_header(); ?>
 	<?php get_template_part('partials/inicial'); ?>
 
-
-
-
-
-
 	<div class="row">
 		<div class="large-6 large-centered columns">
-<?php
-	$args = array('post_type' => 'ncm');
-	$query = new WP_Query($args);
-	if($query->have_posts()):
-	while($query->have_posts()):
-	$query->the_post();
-?>
-	
 
+			<?php
+				$args = array(
+					'post_type' => 'ncm'
+				);
+				$query = new WP_Query($args);
+				if($query->have_posts()):
+				while($query->have_posts()):
+				$query->the_post();
+			?>
 			<span class="ncm"><b>Número da NCM:</b> <?php the_field('numero_da_ncm') ?></span>
-			<br>
-
-				<?php foreach(wp_get_object_terms(get_the_ID(), 'category' ) as $value): ?>
-				<ul>
-					<li><b><?php echo $value->name; ?></b> <?php #echo $value->term_id; ?></li>
-				</ul>
-				<?php endforeach; ?>
-	<hr>
+			<br><hr>
+			<h5><?php wp_list_categories(); ?></h5>
 
 <?php endwhile; wp_reset_postdata();endif; ?>
 	</div>
-	</div>
-
-
-
-
+</div>
 
 <?php get_footer(); ?>
